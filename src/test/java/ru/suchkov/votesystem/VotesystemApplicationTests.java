44918254eca644dbfccbb61019275f0c102c0b66
Денис Сучков <ny1828@yandex.ru>
2020-01-06 20:40:51 +0300

    init project

---
+++ src/test/java/ru/suchkov/votesystem/VotesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.suchkov.votesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
