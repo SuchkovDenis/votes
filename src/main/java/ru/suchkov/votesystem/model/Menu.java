18de15d7dce442bcf3fb21b9d2c7fce7d9d2b025
Денис Сучков <ny1828@yandex.ru>
2020-01-06 20:52:18 +0300

    added annotations to entities

--- src/main/java/ru/suchkov/votesystem/model/Menu.java
+++ src/main/java/ru/suchkov/votesystem/model/Menu.java
@@ -1,4 +1,12 @@
 package ru.suchkov.votesystem.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "menus")
 public class Menu {
 }
--- src/main/java/ru/suchkov/votesystem/model/User.java
+++ src/main/java/ru/suchkov/votesystem/model/User.java
@@ -1,4 +1,12 @@
 package ru.suchkov.votesystem.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
 }
