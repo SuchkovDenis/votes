72aeebcc77de6bc0e8efe847201ccaed612bd31c
Денис Сучков <ny1828@yandex.ru>
2020-01-11 17:25:13 +0300

    Changed HelloWorldController

--- src/main/java/ru/suchkov/votesystem/controller/HelloWorldController.java
+++ src/main/java/ru/suchkov/votesystem/controller/HelloWorldController.java
@@ -1,13 +1,22 @@
 package ru.suchkov.votesystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.suchkov.votesystem.dto.DishDto;
+import ru.suchkov.votesystem.mapper.DishMapper;
+import ru.suchkov.votesystem.model.Dish;
+
+import java.math.BigDecimal;
 
 // todo пониать что за пользователь сейчас
 @RestController
 @CrossOrigin()
 public class HelloWorldController {
+
+	@Autowired
+	DishMapper dishMapper;
 
 	@RequestMapping({ "/hello" })
 	public String hello() {
@@ -15,8 +24,11 @@ public class HelloWorldController {
 	}
 
 	@RequestMapping({ "/helloAdmin" })
-	public String helloAdmin() {
-		return "Hello Admin";
+	public DishDto helloAdmin() {
+		Dish dish = new Dish();
+		dish.setName("Окорок");
+		dish.setPrice(new BigDecimal(12));
+		return dishMapper.dishToDishDto(dish);
 	}
 
 	@RequestMapping({ "/helloUser" })
