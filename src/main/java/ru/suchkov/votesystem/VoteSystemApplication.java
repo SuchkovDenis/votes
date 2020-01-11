1d3fb59f9d1bb32cc1163a545781c2a3a80dfc27
Денис Сучков <ny1828@yandex.ru>
2020-01-11 18:48:46 +0300

    rename main class

--- src/main/java/ru/suchkov/votesystem/VoteSystemApplication.java
+++ src/main/java/ru/suchkov/votesystem/VoteSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VotesystemApplication {
+public class VoteSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VotesystemApplication.class, args);
+		SpringApplication.run(VoteSystemApplication.class, args);
 	}
 
 }
